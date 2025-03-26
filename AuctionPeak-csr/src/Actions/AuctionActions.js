ed69e11b36a9a8513221d14d4c0374e56cfedba7
DipanjanSur <dip20.sur@gmail.com>
2025-03-26 23:15:37 +0530

    remove some files and add some file

--- AuctionPeak-csr/src/Actions/AuctionActions.js
+++
@@ -1,6 +0,0 @@
-const baseUrl = "https://api.themoviedb.org/3/";
-
-export const ATTEND_AUCTION = "Attend Auction"
-export const REMOVE_AUCTION = "Remove Auctiod"
-export const REQUEST_ACCESS_AUCTION = "Request Acess"
-export const ERROr = "error"
